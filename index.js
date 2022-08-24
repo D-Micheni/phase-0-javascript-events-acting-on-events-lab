56a4529245eb271b14df7ebe39b1dd681cdf547a
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-08-24 10:31:09 +0300

    commit tree 61eeb6f85641977636c2b977b2a34fa1103f0f4a

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Your code here
 const dodger = document.getElementById("dodger");
 // dodger.style.backgroundColor = "#000000";
-// dodger.style.backgroundColor = "#FF69B4";
-// dodger.style.bottom = "100px";
-// dodger.style.bottom = "0px";
-// dodger.style.left = "0px";
+//dodger.style.backgroundColor = "#FF69B4";
+//dodger.style.bottom = "100px";
+//dodger.style.bottom = "0px";
+//dodger.style.left = "0px";
 // document.addEventListener("keydown", function (event) {
 //     console.log(event);
 
